933fef631db0103d4984cb171789d03ad38bdbd3
Pep96 <isabelle06pereira@gmail.com>
2026-05-23 16:03:03 -0300

    fix: add missing ThemeCubit import in home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/theme_cubit.dart';
 import '../bloc/todo_bloc.dart';
 import '../bloc/todo_event.dart';
 import '../bloc/todo_state.dart';
